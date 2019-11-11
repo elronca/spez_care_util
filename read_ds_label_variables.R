4dff07a05e31b26a74b6850a457e1b5c99e90fbf
elronca <eronca@gmail.com>
2019-11-11 18:04:33 +0100

    removed stupid error prone regex matching of variables.

--- read_ds_label_variables.R
+++ read_ds_label_variables.R
@@ -6,9 +6,9 @@ library(readxl)
 
 # Make codebooks useable to label variables -------------------------------
 
-
 get_cb_labels <- function(path_cb, my_sheet, my_col_types) {
 
+  
 cb <- read_xlsx(path_cb, sheet = my_sheet, col_types = my_col_types, skip = 1, 
                 .name_repair = ~str_to_lower(str_replace_all(.x, " ", "_"))) %>% 
   
@@ -38,6 +38,7 @@ if( any(names(cb) %in% "suffix") ) {
   cb <- select(cb, var_name = variable_name, var_level, var_label)
 
 }
+
 
 # Get labels for codes from starter and basic module of 2012
 
@@ -71,40 +72,30 @@ cb_17 <- cb_hsr_12 <- get_cb_labels(
 rm("cb_hsr_12", "cb_st_ba_12", "get_cb_labels")
 
 
+
+
 # Read and format data ---------------------------------------------------------------
 
-
-# Function takes variable and codebook and labels the levels of categorical variables
-
-# Read and format data of SwiSCI pathway 2, survey 2012 ----------------------------
-
-# Make variables matchable by select function
-
-# Load whole dataset
+# Function to load and label swisci data
 
 load_swisci_data <- function(path_ds, tp, all_vars, num_vars, cat_vars, codebook) {
   
-  # Make variables one string
-  
-  all_vars <- str_c("^", all_vars, collapse = "|")
-  num_vars <- str_c("^", num_vars, collapse = "|")
-  cat_vars <- str_c("^", cat_vars, collapse = "|")
-  
-  # Read dataset, remove prefix (ts_), and change numeric variables to numeric
+
+    # Read dataset, remove prefix (ts_), and change numeric variables to numeric
   
   my_file <- read_csv2(path_ds, col_types = cols(.default = "c")) %>% 
     rename_all(~str_remove(., str_c("^", tp, "_"))) %>% 
-    select(matches(all_vars)) %>% 
-    mutate_at(vars(matches(num_vars)), as.numeric)
+    select(all_vars) %>% 
+    mutate_at(num_vars, as.numeric)
   
   
-  recode_variables <- function(my_var_name, data, codebook) {
+  recode_variables <- function(my_var_name, my_file, codebook) {
     
-    my_cb <- filter(codebook, str_detect(var_name, my_var_name)) %>% 
+    my_cb <- filter(codebook, var_name %in% my_var_name) %>% 
       pivot_wider(names_from = var_name, values_from = var_level)
     
-    var_coding_in_ds <- pull(select(data, matches(my_var_name)))
-    var_coding_in_cb <- pull(select(my_cb, matches(my_var_name)))
+    var_coding_in_ds <- my_file[[my_var_name]]
+    var_coding_in_cb <- my_cb[[my_var_name]]
     
     matched <- match(var_coding_in_ds, var_coding_in_cb)
     
@@ -113,14 +104,12 @@ load_swisci_data <- function(path_ds, tp, all_vars, num_vars, cat_vars, codebook
   }
   
   
-  cat_vars_sep <- str_split(cat_vars, "\\|") %>% pluck(1)
-  
-  ds_cat_vars <- map_dfc(cat_vars_sep, recode_variables, data = my_file, codebook = codebook) %>% 
-    set_names(str_remove(cat_vars_sep, "\\^")) %>% 
+  ds_cat_vars <- map_dfc(cat_vars, recode_variables, my_file = my_file, codebook = codebook) %>% 
+    set_names(cat_vars) %>% 
     add_column(id_swisci = my_file$id_swisci, .before = 1) %>% 
     mutate_all(str_to_lower)
   
-  ds_num_vars <- select(my_file, id_swisci, matches(num_vars))
+  ds_num_vars <- select(my_file, id_swisci, one_of(num_vars))
   
   full_join(ds_cat_vars, ds_num_vars, by = "id_swisci") %>% 
     full_join(my_file[, c("id_swisci", "medstat")], by = "id_swisci") %>% 
@@ -128,16 +117,21 @@ load_swisci_data <- function(path_ds, tp, all_vars, num_vars, cat_vars, codebook
   
 }
 
+# Load and relabel swisci data of 2012 ------------------------------------------------
 
+all_vars <- read_csv2("data/2019-C-006_2012__2019_10_22.csv", col_types = cols(.default = "c"))
 
-# Load swisci data of 2012 ------------------------------------------------
+all_vars_names <- all_vars %>% names()
 
-my_vars_all <- c("id_swisci", "sex", "age_quest", "sci_type", "sci_degree", 
-                 "time_since_sci", "sci_cause_type", "medstat")
+all_vars %>% select("ts1_income_household")
 
-my_num_vars <- c("age", "time_since_sci")
+my_vars_all <- c("id_swisci", "sex", "age_quest_admin", "sci_type", "sci_degree", 
+                 "time_since_sci", "sci_cause_type", "medstat", "ef_finances",
+                 "income_household")
 
-my_cat_vars <- c("sex", "sci_type", "sci_degree", "sci_cause_type")
+my_num_vars <- c("age_quest_admin", "time_since_sci")
+
+my_cat_vars <- c("sex", "sci_type", "sci_degree", "sci_cause_type", "ef_finances", "income_household")
 
 swisci_12 <- load_swisci_data(path_ds = "data/2019-C-006_2012__2019_10_22.csv",
                             tp = "ts1",
@@ -149,7 +143,8 @@ swisci_12 <- load_swisci_data(path_ds = "data/2019-C-006_2012__2019_10_22.csv",
 rm(cb_12, my_vars_all, my_num_vars, my_cat_vars)
 
 
-# Load swisci data of 2017
+# Load and relabel swisci data of 2017 ------------------------------------------------
+
 
 # There seem to be problems with the data
 
@@ -163,8 +158,12 @@ if(FALSE) {
   
   data_17_prob %>% slice(473) %>% select(ts2_problem_other_1)
   
-  select(data_17_prob, ts2_hc_practitioner, ts2_hc_practitioner_1, ts2_hc_practitioner_num) %>% 
+  select(data_17_prob, id_swisci, ts2_hc_practitioner, ts2_hc_practitioner_1, ts2_hc_practitioner_num) %>% 
     mutate(test_it = if_else(ts2_hc_practitioner == ts2_hc_practitioner_1, 0, 1)) %>% 
+    filter(test_it == 1)
+  
+  select(data_17_prob, id_swisci, id_swisci_1) %>% 
+    mutate(test_it = if_else(id_swisci == id_swisci_1, 0, 1)) %>% 
     filter(test_it == 1)
   
   data_17_prob %>% select(starts_with("ts2_hc_practitioner"))
@@ -172,7 +171,7 @@ if(FALSE) {
 }
 
 
-# Repair dataset
+# Repair and save repaired dataset as csv file
 
 data_17_no_prob <- "data/2019-C-006_2017__2019_10_23.csv" %>% 
   
@@ -187,14 +186,18 @@ data_17_no_prob <- "data/2019-C-006_2017__2019_10_23.csv" %>%
   write_excel_csv2("data/2019-C-006_2017__2019_10_23_repaired.csv")
 
 
-# Load dataset
+# Load and format repaired dataset
+
+all_vars <- read_csv2("data/2019-C-006_2017__2019_10_23_repaired.csv", col_types = cols(.default = "c")) %>% 
+  # names()
+  select(ts2_sex)
 
 my_vars_all <- c("id_swisci", "sex", "age_quest", "sci_type", "sci_degree", 
-                 "time_since_sci", "sci_cause_type", "medstat")
+                 "time_since_sci", "sci_cause_type", "medstat", "ef_finances", "income_household")
 
-my_num_vars <- c("age", "time_since_sci")
+my_num_vars <- c("age_quest", "time_since_sci")
 
-my_cat_vars <- c("sex", "sci_type", "sci_degree", "sci_cause_type")
+my_cat_vars <- c("sex", "sci_type", "sci_degree", "sci_cause_type", "ef_finances", "income_household")
 
 swisci_17 <- load_swisci_data(path_ds = "data/2019-C-006_2017__2019_10_23_repaired.csv",
                               tp = "ts2",
@@ -208,35 +211,34 @@ rm(cb_17, my_vars_all, my_num_vars, my_cat_vars, load_swisci_data, data_17_no_pr
 
 # Combine data of 2012 and 2017 ----------------------------------------------------------
 
+# Variables that have the same name in both datasets
 intersect(names(swisci_12), names(swisci_17))
 
+# Variables that are unique to each dataset
 setdiff(names(swisci_12), intersect(names(swisci_12), names(swisci_17)))
 setdiff(names(swisci_17), intersect(names(swisci_12), names(swisci_17)))
 
-
+# Rename identical variables with different names
 swisci_12 <- rename(swisci_12, age = age_quest_admin)
 swisci_17 <- rename(swisci_17, age = age_quest)
 
+# Reorder variables
 sci <- bind_rows(swisci_12, swisci_17) %>% arrange(as.numeric(id_swisci), tp)
 
 
 # Check factor levels
-
-sci %>% 
-  select_if(is.character) %>% 
+select_if(sci, is.character) %>% 
   select(-c(id_swisci, medstat)) %>% 
   map(unique)
 
 
-# Repair categorical variables
-
+# Repair identical categories with different names
 sci <- mutate(sci, 
               sci_degree = str_remove(sci_degree, " lesion"),
               sci_cause_type = str_remove(sci_cause_type, " sci"))
 
 
 # Change character variables to factors
-
 to_factor <- c("sex", "sci_type", "sci_degree", "sci_cause_type")
 
 sci <- mutate_at(sci, to_factor, as.factor)
