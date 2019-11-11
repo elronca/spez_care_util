b9e5e2fbb3399cfc44954b82125eace1ce13f7f1
elronca <eronca@gmail.com>
2019-11-11 15:57:46 +0100

    Redundencies removed SwiSCI dataset of 2017 repaired

--- read_ds_label_variables.R
+++ read_ds_label_variables.R
@@ -3,97 +3,72 @@ library(tidyverse)
 library(readxl)
 
 
+
 # Make codebooks useable to label variables -------------------------------
 
 
-# Codebooks for SwiSCI pathway 2, 2012 data
+get_cb_labels <- function(path_cb, my_sheet, my_col_types) {
 
-format_codebook <- function(data) {
+cb <- read_xlsx(path_cb, sheet = my_sheet, col_types = my_col_types, skip = 1, 
+                .name_repair = ~str_to_lower(str_replace_all(.x, " ", "_"))) %>% 
   
-  cb <- data %>% 
-    
-    mutate(codes_split =  str_split(codes, "\r\n")) %>% 
-    
-    unnest(codes_split) %>% 
-    
-    mutate(levels_labels_split = str_split(codes_split, "=")) %>% 
-    
-    mutate(var_level = map(levels_labels_split, 1),
-           var_label = map(levels_labels_split, 2)) %>%
-    
-    mutate_at(vars(var_level, var_label), as.character) %>% 
-    
-    mutate_at(vars(var_level, var_label), ~ if_else(. == "NULL", NA_character_, .)) %>% 
-    
-    select(-c(codes, codes_split, levels_labels_split))
+  mutate(codes_split =  str_split(codes, "\r\n")) %>% 
   
-  if( any(names(data) %in% "suffix") ) {
-    
-    mutate(cb, suffix = if_else(is.na(suffix), "", suffix),
-           var_name = str_c(variable_name, suffix))
-    
-  } else {cb}
+  unnest(codes_split) %>% 
   
+  mutate(levels_labels_split = str_split(codes_split, "=")) %>% 
+  
+  mutate(var_level = map(levels_labels_split, 1),
+         var_label = map(levels_labels_split, 2)) %>%
+  
+  mutate_at(vars(var_level, var_label), as.character) %>% 
+  
+  mutate_at(vars(var_level, var_label), ~ if_else(. == "NULL", NA_character_, .)) %>% 
+  
+  select(-c(codes, codes_split, levels_labels_split))
+
+
+if( any(names(cb) %in% "suffix") ) {
+  
+  cb <- mutate(cb, suffix = if_else(is.na(suffix), "", suffix),
+         var_name = str_c(variable_name, suffix))
+  
+} else {cb <- cb}
+  
+  cb <- select(cb, var_name = variable_name, var_level, var_label)
+
 }
-  
 
-# starter & basic
+# Get labels for codes from starter and basic module of 2012
 
-cb_st_ba_12_path <- "data/Pathway2_Codebook_Starter_Basic_2019_04_02.xlsx"
-
-cb_st_ba_12_raw <- read_xlsx(cb_st_ba_12_path, 
-                               sheet = "Starter-Basic", col_types = c("skip", rep("text", 8), "skip"), 
-                               skip = 1, .name_repair = ~str_to_lower(str_replace_all(.x, " ", "_")))
+cb_st_ba_12 <- get_cb_labels(
+  path_cb = "data/Pathway2_Codebook_Starter_Basic_2019_04_02.xlsx", 
+  my_sheet = "Starter-Basic", 
+  my_col_types = c("skip", rep("text", 8), "skip"))
 
 
-cb_st_ba_12_full <- format_codebook(cb_st_ba_12_raw)
+# Get labels for codes from health services research module of 2012
 
-cb_st_ba_12 <- select(cb_st_ba_12_full, var_name = variable_name, var_level, var_label)
-
-
-# Health services research
-
-cb_hsr_12_path <- "data/Pathway2_Codebook_HSR_2013_09_10.xlsx"
-
-cb_hsr_12_raw <- read_xlsx(cb_hsr_12_path, 
-                               sheet = "HSR", col_types = c("skip", rep("text", 8), "skip"), 
-                               skip = 1, .name_repair = ~str_to_lower(str_replace_all(.x, " ", "_")))
-
-
-cb_hsr_12_full <- format_codebook(cb_hsr_12_raw)
-cb_hsr_12 <- select(cb_hsr_12_full, var_name = variable_name, var_level, var_label)
-
-cb_hsr_12 <- filter(cb_hsr_12, var_name == "id_swisci")
+cb_hsr_12 <- get_cb_labels(
+  path_cb = "data/Pathway2_Codebook_HSR_2013_09_10.xlsx", 
+  my_sheet = "HSR", 
+  my_col_types = c("skip", rep("text", 8), "skip"))
 
 
 cb_12 <- bind_rows(cb_st_ba_12, cb_hsr_12)
 
 
+# Get labels for codes from 2017 codebook
 
-
-# Codebook for SwiSCI pathway 2, 2017 data
-
-cb_17_path <- "data/Pathway2_2017_Codebook_2019_10_10.xlsx"
-
-cb_17_path_raw <- read_xlsx(cb_17_path, 
-                              sheet = "Questionnaires", col_types = c("skip", rep("text", 11)), 
-                              skip = 1, .name_repair = ~str_to_lower(str_replace_all(.x, " ", "_")))
-
-cb_17_full <- format_codebook(cb_17_path_raw)
-
-
-cb_17 <- select(cb_17_full, var_name, var_level, var_label)
+cb_17 <- cb_hsr_12 <- get_cb_labels(
+  path_cb = "data/Pathway2_2017_Codebook_2019_10_10.xlsx", 
+  my_sheet = "Questionnaires", 
+  my_col_types = c("skip", rep("text", 11)))
 
 
 # Clean workspace
 
-rm("cb_17_full", "cb_17_path", "cb_17_path_raw", 
-  "cb_hsr_12", "cb_hsr_12_full", "cb_hsr_12_path", "cb_hsr_12_raw", 
-  "cb_st_ba_12", "cb_st_ba_12_full", "cb_st_ba_12_path", "cb_st_ba_12_raw", 
-  "format_codebook")
-
-
-
+rm("cb_hsr_12", "cb_st_ba_12", "get_cb_labels")
 
 
 # Read and format data ---------------------------------------------------------------
@@ -101,180 +76,174 @@ rm("cb_17_full", "cb_17_path", "cb_17_path_raw",
 
 # Function takes variable and codebook and labels the levels of categorical variables
 
-recode_variables <- function(my_var_name, data, codebook) {
-  
-  my_cb <- filter(codebook, str_detect(var_name, my_var_name)) %>% 
-    pivot_wider(names_from = var_name, values_from = var_level)
-  
-  var_coding_in_ds <- pull(select(data, matches(my_var_name)))
-  var_coding_in_cb <- pull(select(my_cb, matches(my_var_name)))
-  
-  matched <- match(var_coding_in_ds, var_coding_in_cb)
-  
-  map_chr(matched, function(x) my_cb$var_label[x])
-  
-  
-}
-
-
 # Read and format data of SwiSCI pathway 2, survey 2012 ----------------------------
 
-
-# Preparations
-
-path_ds <- "data/2019-C-006_2012__2019_10_22.csv"
-
-my_vars_all <- c("id_swisci", "sex", "age_quest", "sci_type", "sci_degree", 
-                 "time_since_sci", "sci_cause_type", "medstat")
-
-my_vars_all <- str_c("^", my_vars_all, collapse = "|")
-
-
-my_num_vars <- c("age", "time_since_sci")
-my_num_vars <- str_c("^", my_num_vars, collapse = "|")
-
-my_cat_vars <- c("sex", "sci_type", "sci_degree", "sci_cause_type")
-my_cat_vars <- str_c("^", my_cat_vars, collapse = "|")
-
-n_cols_ds <- 108
-
+# Make variables matchable by select function
 
 # Load whole dataset
 
-data_12_raw <- read_csv2(path_ds, col_types = str_c(rep("c", n_cols_ds), collapse = ""))
+load_swisci_data <- function(path_ds, tp, all_vars, num_vars, cat_vars, codebook) {
+  
+  # Make variables one string
+  
+  all_vars <- str_c("^", all_vars, collapse = "|")
+  num_vars <- str_c("^", num_vars, collapse = "|")
+  cat_vars <- str_c("^", cat_vars, collapse = "|")
+  
+  # Read dataset, remove prefix (ts_), and change numeric variables to numeric
+  
+  my_file <- read_csv2(path_ds, col_types = cols(.default = "c")) %>% 
+    rename_all(~str_remove(., str_c("^", tp, "_"))) %>% 
+    select(matches(all_vars)) %>% 
+    mutate_at(vars(matches(num_vars)), as.numeric)
+  
+  
+  recode_variables <- function(my_var_name, data, codebook) {
+    
+    my_cb <- filter(codebook, str_detect(var_name, my_var_name)) %>% 
+      pivot_wider(names_from = var_name, values_from = var_level)
+    
+    var_coding_in_ds <- pull(select(data, matches(my_var_name)))
+    var_coding_in_cb <- pull(select(my_cb, matches(my_var_name)))
+    
+    matched <- match(var_coding_in_ds, var_coding_in_cb)
+    
+    map_chr(matched, function(x) my_cb$var_label[x])
+    
+  }
+  
+  
+  cat_vars_sep <- str_split(cat_vars, "\\|") %>% pluck(1)
+  
+  ds_cat_vars <- map_dfc(cat_vars_sep, recode_variables, data = my_file, codebook = codebook) %>% 
+    set_names(str_remove(cat_vars_sep, "\\^")) %>% 
+    add_column(id_swisci = my_file$id_swisci, .before = 1) %>% 
+    mutate_all(str_to_lower)
+  
+  ds_num_vars <- select(my_file, id_swisci, matches(num_vars))
+  
+  full_join(ds_cat_vars, ds_num_vars, by = "id_swisci") %>% 
+    full_join(my_file[, c("id_swisci", "medstat")], by = "id_swisci") %>% 
+    mutate(tp = tp)
+  
+}
 
-data_12 <- data_12_raw %>% 
-  rename_all(~str_remove(., "^ts1_")) %>% 
-  select(matches(my_vars_all)) %>% 
-  mutate_at(vars(matches(my_num_vars)), as.numeric)
 
 
-# Recode and get cateogrical variables
-
-my_cat_vars <- str_split(my_cat_vars, "\\|") %>% pluck(1)
-
-df_cat_vars <- map_dfc(my_cat_vars, recode_variables, data = data_12, codebook = cb_12) %>% 
-  set_names(str_remove(my_cat_vars, "\\^")) %>% 
-  add_column(id_swisci = data_12$id_swisci, .before = 1)
-
-df_cat_vars <- mutate_all(df_cat_vars, str_to_lower)
-
-
-# Get numeric variables
-
-df_num_vars <- select(data_12, id_swisci, matches(my_num_vars))
-
-df_12 <- full_join(df_cat_vars, df_num_vars, by = "id_swisci") %>% 
-  full_join(data_12[, c("id_swisci", "medstat")], by = "id_swisci") %>% 
-  mutate(tp = "ts1")
-
-rm("cb_12", "data_12", "data_12_raw", "df_cat_vars", "df_num_vars", "my_cat_vars", 
-   "my_num_vars", "my_vars_all", "n_cols_ds", "path_ds")
-
-
-# Read and format data of SwiSCI pathway 2, survey 2017 ----------------------------
-
-
-# Preparations
-
-path_ds <- "data/2019-C-006_2017__2019_10_23.csv"
+# Load swisci data of 2012 ------------------------------------------------
 
 my_vars_all <- c("id_swisci", "sex", "age_quest", "sci_type", "sci_degree", 
                  "time_since_sci", "sci_cause_type", "medstat")
 
-my_vars_all <- str_c("^", my_vars_all, collapse = "|")
-
-
 my_num_vars <- c("age", "time_since_sci")
-my_num_vars <- str_c("^", my_num_vars, collapse = "|")
 
 my_cat_vars <- c("sex", "sci_type", "sci_degree", "sci_cause_type")
-my_cat_vars <- str_c("^", my_cat_vars, collapse = "|")
 
-n_cols_ds <- 213
+swisci_12 <- load_swisci_data(path_ds = "data/2019-C-006_2012__2019_10_22.csv",
+                            tp = "ts1",
+                            all_vars = my_vars_all,
+                            num_vars = my_num_vars,
+                            cat_vars = my_cat_vars,
+                            codebook = cb_12)
 
-data_17_raw <- read_csv2(path_ds, col_types = str_c(rep("c", n_cols_ds), collapse = "")) %>% 
-  rename_all(~str_remove(., "^ts2_")) %>% 
-  select(matches(my_vars_all)) %>% 
-  mutate_at(vars(matches(my_num_vars)), as.numeric)
-
-
-# Manually choose variables
-
-data_17 <- select(data_17_raw, -id_swisci_1)
+rm(cb_12, my_vars_all, my_num_vars, my_cat_vars)
 
 
-# Switch to TRUE to show warnings and problems with the dataset
+# Load swisci data of 2017
+
+# There seem to be problems with the data
 
 if(FALSE) { 
-
-data_17_prob <- read_csv2(path_ds, col_types = str_c(rep("c", n_cols_ds), collapse = ""))
-
-problems(data_17_prob)
-
-warnings()
-
-data_17_prob %>% slice(473) %>% select(ts2_problem_other_1)
-
-select(data_17_prob, ts2_hc_practitioner, ts2_hc_practitioner_1) %>% 
-  mutate(test_it = if_else(ts2_hc_practitioner == ts2_hc_practitioner_1, 0, 1)) %>% 
-  filter(test_it == 1)
-
-}
- 
   
+  data_17_prob <- read_csv2("data/2019-C-006_2017__2019_10_23.csv", col_types = cols(.default = "c"))
+  
+  problems(data_17_prob)
+  
+  warnings()
+  
+  data_17_prob %>% slice(473) %>% select(ts2_problem_other_1)
+  
+  select(data_17_prob, ts2_hc_practitioner, ts2_hc_practitioner_1, ts2_hc_practitioner_num) %>% 
+    mutate(test_it = if_else(ts2_hc_practitioner == ts2_hc_practitioner_1, 0, 1)) %>% 
+    filter(test_it == 1)
+  
+  data_17_prob %>% select(starts_with("ts2_hc_practitioner"))
+  
+}
 
 
-# Recode cateogrical variables
+# Repair dataset
 
-my_cat_vars <- str_split(my_cat_vars, "\\|") %>% pluck(1)
+data_17_no_prob <- "data/2019-C-006_2017__2019_10_23.csv" %>% 
+  
+  read_csv2(col_types = cols(.default = "c")) %>% 
+  
+  mutate(ts2_hc_practitioner_num = if_else(ts2_hc_practitioner == ts2_hc_practitioner_1, 
+                                           ts2_hc_practitioner_num, 
+                                           ts2_hc_practitioner_1)) %>% 
+  
+  select(-ts2_hc_practitioner_1, -id_swisci_1) %>% 
+ 
+  write_excel_csv2("data/2019-C-006_2017__2019_10_23_repaired.csv")
 
-df_cat_vars <- map_dfc(my_cat_vars, recode_variables, data = data_17, codebook = cb_17) %>% 
-  set_names(str_remove(my_cat_vars, "\\^")) %>% 
-  add_column(id_swisci = data_17$id_swisci, .before = 1) %>% 
-  mutate_all(str_to_lower)
 
+# Load dataset
 
-# Get numeric variables
+my_vars_all <- c("id_swisci", "sex", "age_quest", "sci_type", "sci_degree", 
+                 "time_since_sci", "sci_cause_type", "medstat")
 
-df_num_vars <- select(data_17, id_swisci, matches(my_num_vars))
+my_num_vars <- c("age", "time_since_sci")
 
+my_cat_vars <- c("sex", "sci_type", "sci_degree", "sci_cause_type")
 
+swisci_17 <- load_swisci_data(path_ds = "data/2019-C-006_2017__2019_10_23_repaired.csv",
+                              tp = "ts2",
+                              all_vars = my_vars_all,
+                              num_vars = my_num_vars,
+                              cat_vars = my_cat_vars,
+                              codebook = cb_17)
 
-# Join data and add additional variables
-
-df_17 <- full_join(df_cat_vars, df_num_vars, by = "id_swisci") %>% 
-  full_join(data_17[, c("id_swisci", "medstat")], by = "id_swisci") %>% 
-  mutate(tp = "ts2")
-
-# Clear workspace
-
-rm("cb_17", "data_17", "data_17_raw", "df_cat_vars", "df_num_vars", "my_cat_vars", 
-   "my_num_vars", "my_vars_all", "n_cols_ds", "path_ds", "recode_variables")
-
+rm(cb_17, my_vars_all, my_num_vars, my_cat_vars, load_swisci_data, data_17_no_prob)
 
 
 # Combine data of 2012 and 2017 ----------------------------------------------------------
 
-intersect(names(df_12), names(df_17))
-setdiff(names(df_12), names(df_17))
+intersect(names(swisci_12), names(swisci_17))
 
-names(df_12)
-names(df_17)
+setdiff(names(swisci_12), intersect(names(swisci_12), names(swisci_17)))
+setdiff(names(swisci_17), intersect(names(swisci_12), names(swisci_17)))
 
-df_12 <- rename(df_12, age = age_quest_admin)
-df_17 <- rename(df_17, age = age_quest)
 
-sci <- bind_rows(df_12, df_17) %>% 
-  
-  arrange(as.numeric(id_swisci), tp) %>% 
-  
-  mutate(sci_degree = str_remove(sci_degree, " lesion"),
-         sci_cause_type = str_remove(sci_cause_type, " sci"))
+swisci_12 <- rename(swisci_12, age = age_quest_admin)
+swisci_17 <- rename(swisci_17, age = age_quest)
+
+sci <- bind_rows(swisci_12, swisci_17) %>% arrange(as.numeric(id_swisci), tp)
+
+
+# Check factor levels
+
+sci %>% 
+  select_if(is.character) %>% 
+  select(-c(id_swisci, medstat)) %>% 
+  map(unique)
+
+
+# Repair categorical variables
+
+sci <- mutate(sci, 
+              sci_degree = str_remove(sci_degree, " lesion"),
+              sci_cause_type = str_remove(sci_cause_type, " sci"))
+
+
+# Change character variables to factors
+
+to_factor <- c("sex", "sci_type", "sci_degree", "sci_cause_type")
+
+sci <- mutate_at(sci, to_factor, as.factor)
 
 
 # Save data and clear workspace -------------------------------------------
 
 save(sci, file = file.path("workspace", "raw_data.Rdata"))
 
-rm("df_12", "df_17", "sci")
+rm("swisci_12", "swisci_17", "to_factor", "sci")
